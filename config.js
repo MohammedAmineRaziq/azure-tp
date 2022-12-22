660fef1e3a46e69a98dfc4a47daa6185624e1a8a
MohammedAmineRaziq <mohammedamineraziq@gmail.com>
2022-12-22 15:45:24 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const config = {};
+config.host =
+  process.env.HOST || "https://mohammed-amine-raziq.documents.azure.com:443/";
+config.authKey =
+  process.env.AUTH_KEY ||
+  "qkWVYm9lsfz6q4zTEeMS0atS1oAxLh2T2JJAMzNpCkA3mNTJgQosZVYqty12pE2QDaju4ZOSwBMUACDb8Zs8eA==";
+config.databaseId = "ToDoList";
+config.containerId = "Items";
+if (config.host.includes("https://localhost:")) {
+  console.log("Local environment detected");
+  console.log(
+    "WARNING: Disabled checking of self-signed certs. Do not have this code in production."
+  );
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  console.log(`Go to http://localhost:${process.env.PORT || "3000"} to
+try the sample.`);
+}
+module.exports = config;
